53c2dab3f0f3b1c961bcdd23ce0ce758f4797b9b
KristianAbad <kpabad@gmail.com>
2026-02-24 18:14:52 -0800

    Javascript fix

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ let guestScore = document.getElementById("guest-score")
 
 function incrementHomeOne(){
     homeCount += 1;
-    homeScore.innerHTML = homeCount;
+    homeScore.textContent = homeCount;
 }
 
 function incrementHomeTwo(){
     homeCount += 2;
-    homeScore.innerHTML = homeCount;
+    homeScore.textContent = homeCount;
 }
 
 function incrementHomeThree(){
     homeCount += 3;
-    homeScore.innerHTML = homeCount;
+    homeScore.textContent = homeCount;
 }
 
 function incrementGuestOne(){
     guestCount += 1;
-    guestScore.innerHTML = guestCount;
+    guestScore.textContent = guestCount;
 }
 
 function incrementGuestTwo(){
     guestCount += 2;
-    guestScore.innerHTML = guestCount;
+    guestScore.textContent = guestCount;
 }
 
 function incrementGuestThree(){
     guestCount += 3;
-    guestScore.innerHTML = guestCount;
+    guestScore.textContent = guestCount;
 
 }
