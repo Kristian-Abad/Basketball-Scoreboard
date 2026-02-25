22f29b7bc1a27ea57040d7a702051c73fab7cf36
KristianAbad <kpabad@gmail.com>
2026-02-24 17:47:01 -0800

    Full project
    This is the project without the stretch goals.

---
+++ index.js
@@ -0,0 +1,36 @@
+
+let homeCount = 0;
+let guestCount = 0;
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score")
+
+function incrementHomeOne(){
+    homeCount += 1;
+    homeScore.textContent = homeCount;
+}
+
+function incrementHomeTwo(){
+    homeCount += 2;
+    homeScore.textContent = homeCount;
+}
+
+function incrementHomeThree(){
+    homeCount += 3;
+    homeScore.textContent = homeCount;
+}
+
+function incrementGuestOne(){
+    guestCount += 1;
+    guestScore.textContent = guestCount;
+}
+
+function incrementGuestTwo(){
+    guestCount += 2;
+    guestScore.textContent = guestCount;
+}
+
+function incrementGuestThree(){
+    guestCount += 3;
+    guestScore.textContent = guestCount;
+
+}
